3571eb169ef8537b8eacd562e1f464b72ae7c66c
IdanMai <idan.maierovicz@gmail.com>
2022-05-19 11:09:00 +0300

    working good

--- static/main.js
+++ static/main.js
@@ -1,14 +1,15 @@
 const activePage = window.location.pathname;
 
 document.querySelectorAll('header a').forEach(link => {
-    if(link.href.includes(activePage))
+    if (link.href.includes(activePage))
         link.classList.add('active-nav');
 })
 
 // animate a twist for the house image
-document.querySelector('.home-page__container img').addEventListener('click', (e)=> {
-    e.target.classList.add('spin')
-    setTimeout(()=> {
-        e.target.classList.remove('spin')
-    }, 2000)
+document.querySelector('.home-page__container img').addEventListener('click', (e) => {
+    console.log(e.target)
+    if (e.target.className === 'spin')
+        e.target.className = ''
+    else
+        e.target.className = 'spin'
 })
